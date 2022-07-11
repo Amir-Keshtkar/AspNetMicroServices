2459248b395ec7ea899522bb940591d13608211a
Amir keshtkar <amir78ahkl@gmail.com>
2022-07-11 16:48:27 +0430

    Basket Api Implemented with Redis Database

---
+++ src/Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿namespace Basket.Api.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; }
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totall = 0;
+                foreach (var cart in Items)
+                {
+                    totall += cart.Price * cart.Quantity;
+                }
+                return totall;
+            }
+        }
+    }
+}
--- src/Services/Basket/Basket.Api/Program.cs
+++ src/Services/Basket/Basket.Api/Program.cs
@@ -1,13 +1,24 @@
+using Basket.Api.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
+
+IConfiguration configuration =app.Configuration;
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
 
 // Configure the HTTP request pipeline.
 if(app.Environment.IsDevelopment()) {
