e2f814b8846d0047c8f2e0f0fe6dd0f8ffc94e6d
Amir keshtkar <amir78ahkl@gmail.com>
2022-07-16 17:20:32 +0430

    Ordering.Api Added , Mediator created

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+        //Task<IEnumerable<Order>> GetOrdersByUserNameAsync(string userName);
+        //Task<IEnumerable<>> DeleteOrdersByUserNameAsync(string userName);
+
+
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Common/ValueObjects.cs
@@ -0,0 +1,29 @@
+﻿namespace Ordering.Domain.Common
+{
+    public abstract class ValueObject
+    {
+        protected abstract IEnumerable<object> GetEqualityComponent();
+        protected static bool EqualOperator(ValueObject left, ValueObject right)
+        {
+            if (left is null ^ right is null)
+            {
+                return false;
+            }
+            return left?.Equals(right) != false;
+        }
+        protected static bool NotEqualOperator(ValueObject left, ValueObject right)
+        {
+            return !(EqualOperator(left, right));
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || obj.GetType() != GetType()) { return false; }
+            var other = (ValueObject)obj;
+            return GetEqualityComponent().SequenceEqual(other.GetEqualityComponent());
+        }
+        public override int GetHashCode()
+        {
+            return GetEqualityComponent().Select(x => x != null ? x.GetHashCode() : 0).Aggregate((x, y) => x ^ y);
+        }
+    }
+}
